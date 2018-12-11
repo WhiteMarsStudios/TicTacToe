eedcc7773d5977f721080aa0c327c92037b638f2
oKatanaaa <whitemarsstudios@gmail.com>
2018-12-11 23:26:20 +0400

    Requested commit. Adding files.

--- scenes/field.gd
+++ scenes/field.gd
@@ -4,13 +4,18 @@ extends Control
 # 1 - client
 const SERVER = 0
 const CLIENT = 1
-const SERVER_IP = 0
-const SERVER_PORT = 0
+var server_ip = null
+var my_ip = null
+const SERVER_PORT = 22
 var peer_type = SERVER
 var turn = true
 var peer
+
+func _ready():
+	self.my_ip = IP.get_local_addresses()[1]
+	print(self.my_ip)
 	
-	
+	$my_ip_info.text = str(self.my_ip)
 	
 func current_player():
 	if (turn):
@@ -22,12 +27,16 @@ func current_player():
 	
 
 func _on_Button_pressed():
+	self.peer_type = SERVER
 	peer = NetworkedMultiplayerENet.new()
-	peer.create_server(1, 2)
+	peer.create_server(22, 2)
 	get_tree().set_network_peer(peer)
 
 
 func _on_Button2_pressed():
-	peer = NetworkedMultiplayerENet.new()
-	peer.create_client(SERVER_IP, SERVER_PORT)
-	get_tree().set_network_peer(peer)
+	self.peer_type = CLIENT
+	self.server_ip = $server_ip_input.text
+	print(self.server_ip)
+	#peer = NetworkedMultiplayerENet.new()
+	#peer.create_client(server_ip, SERVER_PORT)
+	#get_tree().set_network_peer(peer)
